06a7135413f8f51ff9e8498a774b0db7b850f30f
Philippe Rousseau <philippe.rousseau100@gmail.com>
2015-02-11 08:48:38 -0500

    billet

---
+++ Site/javascript/menu.js
@@ -0,0 +1,20 @@
+document.write('\
+				<section class="fluid-sidebar">\
+					<ul class="nav list well">\
+						<li class="nav-header">\
+							Pages\
+		            </li>\
+						<li>\
+							<a href="proposition_recherche.html">Proposition de recherche</a>\
+						</li>\
+					</ul>\
+					<ul class="nav list well">\
+						<li class="nav-header">\
+							Billets\
+						</li>\
+						<li>\
+							<a href="09022015.html">9 février 2015</a>\
+						</li>\
+					</ul>\
+				</section>\
+');
